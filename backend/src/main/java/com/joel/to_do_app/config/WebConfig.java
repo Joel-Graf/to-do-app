90253c5b345639416d4b1c985c5e7dff1ea4e13e
Joel Marcos Graf <55204995+Joel-Graf@users.noreply.github.com>
2024-12-20 17:13:06 -0300

    Update WebConfig.java

--- backend/src/main/java/com/joel/to_do_app/config/WebConfig.java
+++ backend/src/main/java/com/joel/to_do_app/config/WebConfig.java
@@ -1,14 +1,18 @@
 package com.joel.to_do_app.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${BASE_VERCEL_URL}")
+    private String baseVercelUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://to-do-app-seven-ashen.vercel.app", "http://localhost:3000");
+                .allowedOrigins(baseVercelUrl, "http://localhost:3000");
     }
 }
